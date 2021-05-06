6974a568b976e1950c2e6e80e8017c315acee925
NeilS99 <smaugvader99@gmail.com>
2021-05-06 19:25:49 +0530

    Header components

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Header";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+      />
+
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        {/* Logo */}
+      </div>
+      <div className="header__nav"></div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB_6bDNxuoU1c8V5p1jegH3YRxs5HZMDtE",
+  authDomain: "clone-67862.firebaseapp.com",
+  projectId: "clone-67862",
+  storageBucket: "clone-67862.appspot.com",
+  messagingSenderId: "675296057396",
+  appId: "1:675296057396:web:9ebad9388fb866a6b7d261",
+};
