67b19089da5b85e3547f774ec8afb63c4ac54a30
NeilS99 <smaugvader99@gmail.com>
2021-05-09 10:38:29 +0530

    Stripe Functionality

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ import Login from "./Login";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
 import Payment from "./Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51IouxHSCQ9da1cLpQWm3yA9FvT7MyTk0XFSLCc4zcwsKg5JeoFo1FPy1NUiOG8cqkIUKd9IqBnGTrUPhZSZXVzF400X3wZQsDl"
+);
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -44,7 +50,9 @@ function App() {
           </Route>
           <Route path="/payment">
             <Header />
-            <Payment />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/">
             <Header />
